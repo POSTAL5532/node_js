e9d468df6747622d35b4303bdf41abbec100650b
Shcherbachenya Igor <evgeniionegin791@gmail.com>
2022-10-22 15:31:16 +0300

    test

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from 'path';
 import {fileURLToPath} from 'url';
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const simpleToursDBFilePath = `${__dirname}/dev-data/data/tours-simple.json`;
